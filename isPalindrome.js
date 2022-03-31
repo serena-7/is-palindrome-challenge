fb61aaffef00f8aae1e4b320223f64b72a1d185b
Serena Sorensen <serena.sorensen7@gmail.com>
2022-03-31 09:38:29 -0600

    challenge complete

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,1 +1,15 @@
 // Write your code below
+function isPalindrome(str){
+    let backwards = [];
+    for(let i = 0; i < str.length; i++){
+        backwards.unshift(str[i]);
+    }
+    for(let i = 0; i < str.length; i++){
+        if(str[i] !== backwards[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(isPalindrome("noon"));
